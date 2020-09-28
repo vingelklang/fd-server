b8d47b510fc9d109f3b8a8f802e8555c0a852ccd
Boris Kourtoukov <boriskourt@gmail.com>
2020-09-28 14:45:34 +0200

    setup prediction data

---
+++ resources/migrations/20200928130600-add-predictions-table.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE predictions
+(day date PRIMARY KEY,
+ M01 integer[],
+ M02 integer[],
+ M03 integer[]);
--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,10 @@
--- :name create-user! :! :n
--- :doc creates a new user record
-INSERT INTO users
-(id, first_name, last_name, email, pass)
-VALUES (:id, :first_name, :last_name, :email, :pass)
+-- :name insert-day! :! :n
+-- :doc adds the predictions for this day
+INSERT INTO predictions
+(day, M01, M02, M03)
+VALUES (:day, :M01, :M02, :M03)
 
--- :name update-user! :! :n
--- :doc updates an existing user record
-UPDATE users
-SET first_name = :first_name, last_name = :last_name, email = :email
-WHERE id = :id
-
--- :name get-user :? :1
--- :doc retrieves a user record given the id
-SELECT * FROM users
-WHERE id = :id
-
--- :name delete-user! :! :n
--- :doc deletes a user record given the id
-DELETE FROM users
-WHERE id = :id
+-- :name get-day :? :1
+-- :doc retrieves the predictions for this date
+SELECT * FROM predictions
+WHERE day = :day
