e18a6a0a17a6f6508f909df71a35086b9495a4e7
Boris Kourtoukov <boriskourt@gmail.com>
2020-11-16 12:29:20 +0100

    All parsers finished

--- src/clj/fd_server/models/corona_pred.clj
+++ src/clj/fd_server/models/corona_pred.clj
@@ -1,1 +1,17 @@
-(ns fd-server.models.corona-pred)
+(ns fd-server.models.corona-pred
+  (:require
+    [clojure.string :as string]
+    [tick.alpha.api :as t]
+    [tupelo.parse :as tp]
+    [tupelo.string :as ts]))
+
+(defn get-today []
+  (let [file (rest (string/split-lines (string/trim (slurp "resources/data/corona_pred.txt"))))
+        data (map #(-> % (string/split #":") second string/trim tp/parse-double) file)
+        tomorrow (t/tomorrow)
+        with-days (map-indexed (fn add-time [idx d]
+                                 [(t/+ tomorrow (t/new-period idx :days)) d])
+                               data)
+        sorted-days (apply sorted-map with-days)]
+    sorted-days))
+
---
+++ src/clj/fd_server/models/covid_19_pred.clj
@@ -0,0 +1,22 @@
+(ns fd-server.models.covid-19-pred
+  (:require
+    [clojure.string :as string]
+    [tick.alpha.api :as t]
+    [tupelo.parse :as tp]
+    [tupelo.string :as ts]))
+
+(defn get-today []
+  (let [file (drop 3 (string/split-lines (string/trim (slurp "resources/data/covid-19-pred.txt"))))
+        date-plus-val (map #(-> % (string/split #":")) file)
+        with-days (mapv (fn add-time [[t d]]
+                          (let [[day month year] (mapv string/trim (-> t string/trim (string/split #" ")))
+                                date-of-value (t/new-date (tp/parse-int year)
+                                                          (.getValue (t/month month))
+                                                          (tp/parse-int day))]
+                            [date-of-value (tp/parse-double (string/trim d))]))
+                       date-plus-val)
+        tomorrow (t/tomorrow)
+        end-day (t/+ (t/tomorrow) (t/new-period 29 :days))
+        next-thirty (filter #(t/<= tomorrow (first %) end-day) with-days)
+        sorted-days (apply sorted-map (flatten next-thirty))]
+    sorted-days))
